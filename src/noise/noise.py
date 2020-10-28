4d5175a0fb537b0261fa28616262c0d0f525da79
georg3tom <georg3tom@gmail.com>
2020-10-28 21:17:29 +0530

    add noise variations to images

---
+++ src/noise/noise.py
@@ -0,0 +1,114 @@
+from copy import deepcopy
+from random import randint, random
+import os
+
+import numpy as np
+import cv2
+
+from .errors import ImageNotFoundError, InvalidImageError
+
+
+class Noise:
+    """
+    To add noise to an image image
+    """
+
+    def __init__(self, img_path):
+        self.img_path = img_path
+
+        filename = os.path.basename(img_path)
+        name, ext = os.path.splitext(filename)
+
+        self.edits = [name]
+        self.ext = ext
+
+        self.img = self.imread(img_path)
+
+    @staticmethod
+    def imread(img_path):
+        """
+        Raises exception if file doesn't exist or is invalid
+        Returns the image if valid
+        """
+        if not os.path.exists(img_path):
+            raise ImageNotFoundError(f"Image {img_path} could'nt be located")
+
+        img = cv2.imread(img_path)
+
+        if img is None:
+            raise InvalidImageError(f"Image {img_path} could'nt be loaded")
+
+        return img
+
+   
+    def salt(self, prob=0.08):
+        """
+        Adds salt noise to the image
+        """
+        h, w, c = self.img.shape
+        for i in range(h):
+            for j in range(w):
+                if random() < prob:
+                    self.img[i, j] = 255
+
+        self.edits.append(f"salt:{prob}")
+        return self
+
+    def pepper(self, prob=0.08):
+        """
+        Adds pepper noise to the image
+        """
+        h, w, c = self.img.shape
+        for i in range(h):
+            for j in range(w):
+                if random() < prob:
+                    self.img[i, j] = 0
+
+        self.edits.append(f"pepper:{prob}")
+        return self
+
+    def gaussian(self, var = 0.1, mean = 0):
+        """
+        Adds gaussian noise to the image
+        """
+        h, w, c = self.img.shape
+        sigma = var**0.5
+        gauss = np.random.normal(mean,sigma,self.img.shape)
+        gauss = gauss.reshape(self.img.shape)
+        noisy = self.img + gauss
+        self.img = noisy
+        return self
+
+    def speckle(self):
+        return self
+      
+    def poisson(self):
+        return self
+
+    def patchSupression(self):
+        return self
+
+
+     def copy(self):
+        """
+        Returns a copy of this
+        """
+        return deepcopy(self)
+
+    def write(self, name=None, ext=None, directory=None):
+        """
+        Writes the image to disk
+        """
+        delim = "_"
+        if name is None:
+            name = delim.join(self.edits) + delim
+        if directory is not None:
+            name = os.path.join(directory, name)
+        if ext is None:
+            ext = self.ext
+
+        return cv2.imwrite(name + ext, self.img)
+
+
+if __name__ == "__main__":
+    Noise("./inp.jpg").salt().pepper().write()
