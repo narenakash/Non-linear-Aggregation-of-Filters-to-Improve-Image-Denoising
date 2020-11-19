3f8c15a698e559b0930a78a64ec6c00a8d66fb57
georg3tom <georg3tom@gmail.com>
2020-11-19 18:11:37 +0530

    some fix

--- src/cobramachine.py
+++ src/cobramachine.py
@@ -16,12 +16,22 @@ class CobraMachine:
 
     def predict(self, img):
         self.denoisedIMG = []
-        count = 1 if len(img.shape)==1 else img.shape[0]
-        for i in range(count):
-            if len(img.shape)==1:
-                img2 = img.reshape((2*self.size+1,2*self.size+1))
+
+        if len(img.shape)==1:
+            img2 = img.reshape((2*self.size+1,2*self.size+1))
+            dn = Denoise(img2)
+            ret = dn.denoiseNAME(self.denoise)
+
+            if 0 < self.size :
+                self.denoisedIMG.append([ret[self.size,self.size]])
             else:
-                img2 = img[i].reshape((2*self.size+1, 2*self.size+1))
+                self.denoisedIMG.append([ret])
+
+            return self.denoisedIMG
+
+        for i in range(img.shape[0]):
+
+            img2 = img[i].reshape((2*self.size+1, 2*self.size+1))
             dn = Denoise(img2)
             ret = dn.denoiseNAME(self.denoise)
             
